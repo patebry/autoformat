0233c1d8a02d14586ab3e7e75c8b2464fc2ad82b
Pate Bryant <patebry@gmail.com>
2020-09-11 20:35:44 -0400

    cleaned up dependencies

--- index.js
+++ index.js
@@ -3,29 +3,39 @@ const watch = require('watch')
 const { exec } = require('child_process')
 
 watch.watchTree(
+
   '.',
   {
     interval: 0.1,
     ignoreDotFiles: true,
     ignoreDirectoryPattern: /^node_modules.*/
   },
-  function(f, curr, prev) {
+  function (f, curr, prev) {
     if (typeof f == 'object' && prev === null && curr === null) {
-      // Finished walking the tree
+      // nothing
     } else if (prev === null) {
       // f is a new file
-      // console.log("new", f)
+      console.log(`${f} created`)
     } else if (curr.nlink === 0) {
       // f was removed
-      // console.log("removed", f)
+      console.log(`${f} removed`)
     } else {
       // f was changed
-      exec(`prettier --check ./${f}`).stdout.on('data', data => {
+      const path = `./${f}`
+      let commands = {
+        check: `prettier --check ${path}`,
+        fix: `prettier --write ${path}`
+      }
+      const check = exec(commands.check).stdout
+      console.log(commands.check)
+
+      check.on('data', data => {
         if (data.includes('Forgot to run Prettier?')) {
           console.log(`formatting ${f}`)
-          exec(`prettier --write ./${f}`)
+          exec(commands.fix)
         }
       })
+      check.on('error', e => console.error(e))
     }
   }
 )
